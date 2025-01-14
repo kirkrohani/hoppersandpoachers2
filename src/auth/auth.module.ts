31ebc5b06d9af953f8cd6a1f4beb74d0508beb45
kirkrohani <kirk.rohani@gmail.com>
2025-01-14 15:26:04 -0600

    added nestjs/jwt lib created jwt.config file added jwtConfig, jwtModule and Config Modules to Auth Module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,6 +6,9 @@ import { UsersModule } from 'src/users/users.module';
 import { HashingProvider } from './providers/hashing.provider';
 import { BcryptProvider } from './providers/bcrypt.provider';
 import { SignInProvider } from './providers/sign-in.provider';
+import { ConfigModule } from '@nestjs/config';
+import jwtConfig from './config/jwt.config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [AuthController],
@@ -17,7 +20,11 @@ import { SignInProvider } from './providers/sign-in.provider';
     },
     SignInProvider,
   ],
-  imports: [forwardRef(() => UsersModule)],
+  imports: [
+    forwardRef(() => UsersModule),
+    ConfigModule.forFeature(jwtConfig),
+    JwtModule.registerAsync(jwtConfig.asProvider()),
+  ],
   exports: [AuthService, HashingProvider],
 })
 export class AuthModule {}
