83b2810545f37aec2830c4eb5d6599934d1f3311
kirkrohani <kirk.rohani@gmail.com>
2025-01-17 12:05:24 -0600

    added env validations for the JWT env vars

--- src/config/env.validation.ts
+++ src/config/env.validation.ts
@@ -8,4 +8,8 @@ export default Joi.object({
   DB_USER: Joi.string().required(),
   DB_PASSWORD: Joi.string().required(),
   PROFILE_API_KEY: Joi.string().required(),
+  JWT_SECRET: Joi.string().required(),
+  JWT_TOKEN_AUDIENCE: Joi.string().required(),
+  JWT_TOKEN_ISSUER: Joi.string().required(),
+  JWT_ACCESS_TOKEN_TTL: Joi.number().default(3600),
 });
